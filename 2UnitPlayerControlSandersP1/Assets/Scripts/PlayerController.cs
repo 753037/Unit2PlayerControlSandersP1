6859551eff3c218cc81ef1d766679506a0e4600f
753037 <753037@student.dusd.net>
2023-03-06 09:50:48 -0800

    Started and completed lesson 2.3 in my unit 2 player control

--- 2UnitPlayerControlSandersP1/Assets/Scripts/PlayerController.cs
+++ 2UnitPlayerControlSandersP1/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10;
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
 
     // Update is called once per frame
     void Update()
@@ -22,5 +24,11 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
